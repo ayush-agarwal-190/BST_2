821765e506238fe71e189ca004f1eacf809a101a
Ayush Agarwal <95439195+ayush-agarwal-190@users.noreply.github.com>
2022-07-30 11:46:05 +0530

    Add files via upload

---
+++ identicalBST.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left,*right;
+    Node(int val){
+data=val;
+left=NULL;
+right=NULL;
+    }
+};
+bool isIdentical(Node* root1,Node* root2){
+    if(root1==NULL && root2==NULL){
+        return true;
+    }
+    else if(root1==NULL||root2==NULL){
+        return false;
+    }
+    else{
+        bool cond1=root1->data==root2->data;
+        bool cond2=isIdentical(root1->left,root2->left);
+        bool cond3=isIdentical(root1->left,root2->left);
+
+        if(cond1 && cond2 && cond3){
+            return true;
+        }
+        return false;
+    }
+
+}
+int main(){
+    Node* root= new Node(2);
+    root->left=new Node(1);
+    root->right=new Node(3);
+    // root->left->left=new Node(5);
+    // root->left->right=new Node(10);
+    Node* root1= new Node(6);
+    root1->left=new Node(3);
+    root1->right=new Node(10);
+
+if(isIdentical(root,root1))
+cout<<" BST identical";
+else
+cout<<" BST not identical";
+
+    return 0;
+}
